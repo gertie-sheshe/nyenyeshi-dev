66c2f615ffd93499e80e2cd1535b0fc34653d73f
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-07-08 16:22:02 +0300

    (chore) readme + routing

---
+++ src/layouts/layout.js
@@ -0,0 +1,14 @@
+import Header from "@blog/layouts/Header";
+import Footer from "@blog/layouts/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
