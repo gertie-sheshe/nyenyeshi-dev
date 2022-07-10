0741726a796813a5ad5b6d61fd085dcd314a9b7d
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-07-10 18:03:50 +0300

    (feat) order by desc

--- src/utils/sanitycms.js
+++ src/utils/sanitycms.js
@@ -2,21 +2,21 @@ import { sanityClient } from "sanity";
 import { groq } from "next-sanity";
 
 export const fetchAllPosts = async () => {
-  const postsQuery = groq`* [_type == "post"]`;
+  const postsQuery = groq`* [_type == "post"] | order(publishedAt desc)`;
   const posts = await sanityClient.fetch(postsQuery);
 
   return posts;
 };
 
 export const fetchCurrentPost = async (slug) => {
-  const postQuery = groq`* [_type == "post" && slug.current == "${slug}"]`;
+  const postQuery = groq`* [_type == "post" && slug.current == "${slug}"] | order(publishedAt desc)`;
   const post = await sanityClient.fetch(postQuery);
 
   return post;
 };
 
 export const fetchCategoryPosts = async (category) => {
-  const postsQuery = groq`* [_type == "post" && '${category}' in categories[]->title]`;
+  const postsQuery = groq`* [_type == "post" && '${category}' in categories[]->title] | order(publishedAt desc)`;
   const posts = await sanityClient.fetch(postsQuery);
 
   return posts;
