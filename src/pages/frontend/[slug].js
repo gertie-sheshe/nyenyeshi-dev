3cd137dafa81c6ea092b0edf09393a973988aabd
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-08-29 20:43:14 +0300

    (feat) add og urls in all pages

--- src/pages/frontend/[slug].js
+++ src/pages/frontend/[slug].js
@@ -1,8 +1,38 @@
 import Article from "@blog/components/Article";
+import Head from "next/head";
+
 import { fetchCategoryPosts, fetchCurrentPost } from "@blog/utils/sanitycms";
 
 export default function Index({ post }) {
-  return <Article post={post} />;
+  const { title, summary: description } = post;
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:creator" content="@gertiesheshe" key="twhandle" />
+
+        <meta
+          property="og:site_name"
+          content="Gertrude Nyenyeshi's Technical Blog"
+          key="ogsitename"
+        />
+        <meta property="og:title" content={title} key="ogtitle" />
+        <meta property="og:description" content={description} key="ogdesc" />
+        <meta
+          property="og:image"
+          content="https://gdurl.com/ardr"
+          key="ogimage"
+        />
+        <title>{title}</title>
+      </Head>
+      <Article post={post} />
+    </>
+  );
 }
 
 export const getStaticPaths = async (context) => {
